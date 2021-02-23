c1ac42c9d855b9024951e99d0dc5cc0ae8681409
Pholist <64503848+Pholist@users.noreply.github.com>
2021-02-23 17:59:39 +0100

    no hover effect in forms

--- src/web/servlets/RedirectServlet.java
+++ src/web/servlets/RedirectServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/")
+@WebServlet("/aa")
 public class RedirectServlet extends HttpServlet {
 	
 	
